149553acc8999709be7815a762db3a10c5e3c33a
Matthias Männich <matthias.maennich@xing.com>
2014-09-24 11:43:37 +0200

    Override device_chosen to fall back to choosing a device.

--- lib/israkel/tasks.rb
+++ lib/israkel/tasks.rb
@@ -44,42 +44,42 @@ module ISRakel
     def define_allow_addressbook_access_task
       desc "Allow AdressBook access (via BUNDLE_ID environment variable)"
       task "#{name}:allow_addressbook_access" do
-        @device_chosen.allow_addressbook_access(bundle_id)
+        device_chosen.allow_addressbook_access(bundle_id)
       end
     end
 
     def define_allow_gps_access_task
       desc "Allow GPS access (via BUNDLE_ID environment variable)"
       task "#{name}:allow_gps_access" do
-        @device_chosen.allow_gps_access(bundle_id)
+        device_chosen.allow_gps_access(bundle_id)
       end
     end
 
     def define_allow_photos_access_task
       desc "Allow Photos access (via BUNDLE_ID environment variable)"
       task "#{name}:allow_photos_access" do
-        @device_chosen.allow_photos_access(bundle_id)
+        device_chosen.allow_photos_access(bundle_id)
       end
     end
 
     def define_reset_task
       desc "Reset content and settings of the iPhone Simulator"
       task "#{name}:reset" do
-        @device_chosen.reset
+        device_chosen.reset
       end
     end
 
     def define_set_language_task
       desc "Set the system language (via IOS_LANG environment variable)"
       task "#{name}:set_language" do
-        @device_chosen.set_language(ios_lang)
+        device_chosen.set_language(ios_lang)
       end
     end
 
     def define_start_task
       desc "Start the iPhone Simulator"
       task "#{name}:start" do
-        @device_chosen.start
+        device_chosen.start
       end
     end
 
@@ -88,6 +88,10 @@ module ISRakel
       task "#{name}:stop" do
         Device.stop
       end
+    end
+
+    def device_chosen
+      @device_chosen || select_device
     end
 
     def select_device
